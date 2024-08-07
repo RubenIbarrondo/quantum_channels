7d5de931b004267334833afe91e49c6f2d48d63d
RubenIbarrondo <rubenibarrondo@gmail.com>
2024-08-07 15:52:54 +0200

    Add maximally entangled state.

--- src/pyqch/state_families.py
+++ src/pyqch/state_families.py
@@ -8,7 +8,7 @@ specific families. Currently under development.
 
 import numpy as np
 
-def computational_basis(dim: int, index: int) -> np.ndarray:
+def computational_basis(dim: int, index: int, as_density_matrix: bool = False) -> np.ndarray:
     """
     Generates a quantum state in the computational basis.
 
@@ -18,12 +18,45 @@ def computational_basis(dim: int, index: int) -> np.ndarray:
         Dimension of the Hilbert space.
     index : int
         Index of the desired basis state.
+    as_density_matrix: bool
+        Whether the state is returned as a density matrix, by default False.
 
     Returns
     -------
     np.ndarray
         The quantum state in the computational basis.
     """
-    state = np.zeros(dim, dtype=complex)
-    state[index] = 1
-    return state
+    if not as_density_matrix:
+        state = np.zeros(dim, dtype=complex)
+        state[index] = 1
+        return state
+    else:
+        state = np.zeros((dim, dim), dtype=complex)
+        state[index, index] = 1
+        return state
+    
+
+def maximally_entangled(dim: int, as_density_matrix: bool = False) -> np.ndarray:
+    """
+    Generates a maximally entangled state in the computational basis.
+
+    Parameters
+    ----------
+    dim : int
+        Dimension of the Hilbert space.
+    as_density_matrix : bool, optional
+        Whether the state is returned as a density matrix, by default False.
+
+    Returns
+    -------
+    np.ndarray
+        The maximally entangled state.
+    """
+    
+    if not as_density_matrix:
+        state = np.identity(dim, dtype=complex).reshape((dim**2,)) / np.sqrt(dim)
+        return state
+    else:
+        state_vec = maximally_entangled(dim, as_density_matrix=False)
+        state = np.outer(state_vec, state_vec.conj())
+        return state
--- test/test_state_families.py
+++ test/test_state_families.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class TestStateFamilies(unittest.TestCase):
 
-    def test_computational_basis(self):
+    def test_computational_basis_pure(self):
         from  src.pyqch.state_families import computational_basis
         from  src.pyqch.predicates import is_density_matrix
 
@@ -20,3 +20,55 @@ class TestStateFamilies(unittest.TestCase):
 
         rho = np.outer(psi, psi.T.conj())
         self.assertTrue(is_density_matrix(rho))
+    
+
+    def test_computational_basis_mixed(self):
+        from  src.pyqch.state_families import computational_basis
+        from  src.pyqch.predicates import is_density_matrix
+
+        dim = 3
+        index = dim//2
+
+        psi = computational_basis(dim, index, as_density_matrix=True)
+
+        self.assertEqual(psi.dtype, complex)
+        self.assertEqual(psi.shape, (dim, dim))
+
+        self.assertAlmostEqual(psi[index, index], 1)
+        self.assertAlmostEqual(psi[0, 1], 0)
+
+        self.assertTrue(is_density_matrix(psi))
+
+
+    def test_maximally_entangled_pure(self):
+        from  src.pyqch.state_families import maximally_entangled
+        from  src.pyqch.predicates import is_density_matrix
+
+        dim = 3
+
+        omega = maximally_entangled(dim)
+
+        self.assertEqual(omega.dtype, complex)
+        self.assertEqual(omega.shape, (dim**2,))
+
+        self.assertAlmostEqual(omega[0], 1/np.sqrt(dim))
+        self.assertAlmostEqual(np.sum(np.abs(omega)**2), 1)
+
+        rho = np.outer(omega, omega.T.conj())
+        self.assertTrue(is_density_matrix(rho))
+    
+
+    def test_maximally_entangled_mixed(self):
+        from  src.pyqch.state_families import maximally_entangled
+        from  src.pyqch.predicates import is_density_matrix
+
+        dim = 3
+
+        omega = maximally_entangled(dim, as_density_matrix=True)
+
+        self.assertEqual(omega.dtype, complex)
+        self.assertEqual(omega.shape, (dim**2, dim**2))
+
+        self.assertAlmostEqual(omega[0, 0], 1 / dim)
+       
+        self.assertTrue(is_density_matrix(omega))
